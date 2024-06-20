c3f43c811e4bd17383c0b999c17d4224ffa5601c
raeleus <raymond.ray3k@gmail.com>
2024-06-19 18:26:02 -0700

    Added Splash

--- core/src/main/java/com/ray3k/badforce2/Core.java
+++ core/src/main/java/com/ray3k/badforce2/Core.java
@@ -1,11 +1,46 @@
 package com.ray3k.badforce2;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.esotericsoftware.spine.SkeletonRenderer;
+import com.ray3k.badforce2.screens.SplashScreen;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Core extends Game {
+    public static final float GAME_WIDTH = 1024;
+    public static final float GAME_HEIGHT = 576;
+    public static TextureAtlas textureAtlas;
+    public static Skin skin;
+    public static SkeletonRenderer skeletonRenderer;
+
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        textureAtlas = new TextureAtlas(Gdx.files.internal("textures/textures.atlas"));
+        skin = new Skin(Gdx.files.internal("ui/skin.json"), textureAtlas);
+        setScreen(new SplashScreen());
+    }
+
+    public static void onChange(Actor actor, Runnable runnable) {
+        actor.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                runnable.run();
+            }
+        });
+    }
+
+    public static void onClick(Actor actor, Runnable runnable) {
+        actor.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                runnable.run();
+            }
+        });
     }
 }
--- core/src/main/java/com/ray3k/badforce2/screens/SplashScreen.java
+++ core/src/main/java/com/ray3k/badforce2/screens/SplashScreen.java
@@ -1,20 +1,55 @@
 package com.ray3k.badforce2.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import static com.ray3k.badforce2.Core.*;
 
 public class SplashScreen extends ScreenAdapter {
+    private Stage stage;
+    private FitViewport viewport;
+
     @Override
     public void show() {
+        viewport = new FitViewport(GAME_WIDTH, GAME_HEIGHT);
+        stage = new Stage(viewport);
+        Gdx.input.setInputProcessor(stage);
 
+        var root = new Table();
+        root.setFillParent(true);
+        root.setTouchable(Touchable.enabled);
+        stage.addActor(root);
+
+        onClick(root, () -> {
+            
+        });
+
+        var label = new Label("CLICK TO PLAY", skin);
+        root.add(label);
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
+        stage.act();
+        stage.draw();
+    }
 
+    @Override
+    public void dispose() {
+        stage.dispose();
     }
 }
