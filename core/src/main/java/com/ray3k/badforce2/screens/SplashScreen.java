d1af8cc291c5823ba38dc0f0151c0cbd6d325c79
raeleus <raymond.ray3k@gmail.com>
2024-06-19 17:54:31 -0700

    Updated Gradle project.

---
+++ core/src/main/java/com/ray3k/badforce2/screens/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.ray3k.badforce2.screens;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class SplashScreen extends ScreenAdapter {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+}
